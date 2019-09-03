a4a5535ff0f44458bd715cbed72a302a609f30b6
Salvador <salgonzolanda@gmail.com>
2019-09-03 14:25:40 -0700

    mvp

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './App.css';
+
+
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      user: {},
+      followers: []
+    };
+  }
+
+componentDidMount(){
+  fetch('https://api.github.com/users/sgonzalez44')
+  .then(res => res.json())
+  .then(data => this.setState({ user: data }));
+  fetch('https://api.github.com/users/sgonzalez44/followers')
+  .then(res => res.json())
+  .then(data => this.setState({ followers: data }));
+
+}
+componentDidUpdate() {
+  console.log(this.state);
+}
+
+  render(){
+    return(
+  <div className="App">
+  <UserCard user={this.state.user} followers={this.state.followers} />
+  </div>
+    )
+  }
+}
+function UserCard(props) {
+return (
+  <div>
+    <h2>{props.user.login}</h2>
+    <p>{props.user.location}</p>
+    <p>{props.user.url}</p>
+    <div>
+    {props.followers.map(follower => (
+    <div key={follower.id}>{follower.login}</div>
+  ))}
+  </div>
+  </div>
+);
+
+}
+
+export default App;
